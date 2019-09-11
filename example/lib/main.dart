e611bbd0368bfb8d6bd193d0db07b2ea81932cd3
bangwang <79408854@qq.com>
2019-09-11 15:49:00 +0800

    初始化umeng接口

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
-import 'package:umeng_plugin/umeng_plugin.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
@@ -22,22 +20,6 @@ class _MyAppState extends State<MyApp> {
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await UmengPlugin.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
--- test/umeng_plugin_test.dart
+++ test/umeng_plugin_test.dart
@@ -1,21 +1,4 @@
 import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:umeng_plugin/umeng_plugin.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('umeng_plugin');
-
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await UmengPlugin.platformVersion, '42');
-  });
 }
